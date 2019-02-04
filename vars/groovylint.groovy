af537dfe0bfb7a533291a67b0fb23bbef8359a39
nre <nre@ableton.com>
2019-02-04 14:26:39 +0100

    fixup! Add copyright to source files

--- vars/groovylint.groovy
+++ vars/groovylint.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ableton AG, Berlin. All rights reserved.
+ * Copyright (c) 2019 Ableton AG, Berlin. All rights reserved.
  *
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.

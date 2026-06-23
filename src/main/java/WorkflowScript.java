8aef0e484c5dd67e220cce555bb6d6d1c3b1e5f0
nre <nre@ableton.com>
2026-06-23 11:21:54 +0200

    Make WorkflowScript implement Serializable
    The actual WorkflowScript instance on a Jenkins controller is also
    Serializable. Without this interface, we will see false positives from
    CodeNarc when analyzing Jenkinsfiles:
    
    > ExpressionInCpsMethodNotSerializable: Variable <whatever> is not
    > Serializable and used in CPS transformed code
    
    Should have been done in 7f39e9b.

--- src/main/java/WorkflowScript.java
+++ src/main/java/WorkflowScript.java
@@ -5,6 +5,8 @@
  * license that can be found in the LICENSE file.
  */
 
+import java.io.Serializable;
+
 /**
  * Stub for the implicit base class of Jenkins Pipeline scripts.
  *
@@ -17,7 +19,7 @@
  * This stub class gives us a {@code WorkflowScript} name to put in the classpath to
  * satisfy type resolution for CodeNarc. The class itself is never executed.
  */
-public class WorkflowScript extends groovy.lang.Script {
+public class WorkflowScript extends groovy.lang.Script implements Serializable {
     @Override
     public Object run() {
         throw new RuntimeException("Executing this class is not supported");
